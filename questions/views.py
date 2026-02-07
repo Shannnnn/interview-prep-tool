78702017c3e9e9a1e0be6a6035f2491914988101
Shannnnn <sacanoy16@gmail.com>
2026-02-08 00:57:26 +0800

    feat: added question list view and HTML template

--- questions/views.py
+++ questions/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Question
 
-# Create your views here.
+def question_list(request):
+    # 1. Fetch all questions from the database
+    all_questions = Question.objects.all()
+    
+    # 2. Package them into a 'context' dictionary
+    context = {
+        'questions': all_questions
+    }
+    
+    # 3. Send the data to the HTML template
+    return render(request, 'questions/question_list.html', context)
