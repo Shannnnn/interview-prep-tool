4d3060e44a5889055dd84fbf052a29b79e63820b
Shannnnn <sacanoy16@gmail.com>
2026-02-08 00:51:52 +0800

    admin: register Question model in django admin

--- questions/admin.py
+++ questions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Question
 
-# Register your models here.
+# This tells Django: "Show the Question table in the Admin panel"
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category') # Show these columns in the list view
+    list_filter = ('category',)          # Add a sidebar to filter by category
